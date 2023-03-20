237bf01d7871378d6488f7882b0af83068e8528f
Beyker Estrada <beyker.chirino.estrada@gmail.com>
2023-03-20 14:14:07 -0300

    Cambios en app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import { Navbar } from "./components/Navbar/Navbar";
-import Hero from './components/Hero/Hero';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ItemCount from './components/ItemCount/ItemCount';
+
 
 function App() {
   return (
@@ -12,8 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={
           <div>
-            {/* Se deja comentado el hero por esta vez para ir trabajando poco a poco en el, Por ahora me interesa mostrar los componentes necesarios para el desafio */}
-            {/* <Hero/> */} 
+            {/* Eliminé el hero por esta vez para ir trabajando poco a poco en el, Por ahora me interesa mostrar los componentes necesarios para el desafio */}
             <ItemListContainer/>
           </div>
         }/>
